0b699acaf00f3f1d29db127ba0be97d7c499f276
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-08-05 10:23:44 +0200

    Rust personal card

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use std::collections::HashMap;
+use colored::*;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fmt::format;
 
 fn main() {
-    let mut charMap = HashMap::new();
-
-    let name = "Hannah";
-
-    for c in name.chars() {
-        let x = charMap.entry(c).or_insert(0);
-        *x += 1;
+    let file = std::fs::read_to_string("me.json");
+    match file {
+        Ok(f) => {
+            let me: Me = serde_json::from_str(&f).unwrap();
+            let result = format!(
+                "name = {} \nrole = {} \ngithub = {} \ntwitter = {} \nlinkedin = {} \n",
+                me.name.blue(),
+                me.role.green(),
+                me.github.red(),
+                me.twitter.cyan(),
+                me.linkedin.magenta()
+            );
+            println!("{}", result);
+        }
+        Err(e) => {
+            println!("{} {}", "me.json not found".red(), e);
+        }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Me {
+    name: String,
+    role: String,
+    github: String,
+    twitter: String,
+    linkedin: String,
 }
